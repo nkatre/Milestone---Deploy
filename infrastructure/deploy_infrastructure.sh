b277f770093e4b2e167764884af93578dbefe39a
maxlpy <maxlipengyu@gmail.com>
2015-04-15 22:41:34 +0000

    add automatic deployment commands

--- infrastructure/deploy_infrastructure.sh
+++ infrastructure/deploy_infrastructure.sh
@@ -1,10 +1,10 @@
-supt-get update
-sudo apt-get install nodejs y
-sudo apt-get install npm y
-sudo npm install y
-sudo npm install redis y
-sudo apt-get install npm
-sudo apt-get install node
-sudo apt-get install nodejs-legacy
-sudo apt-get install redis-server
+sudo apt-get update
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo npm install -y
+sudo npm install redis -y
+sudo apt-get install npm -y
+sudo apt-get install node -y
+sudo apt-get install nodejs-legacy -y
+sudo apt-get install redis-server -y
 
